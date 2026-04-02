aeb3629f807d9be3f63946d7fbafd5793c29c3d6
Konstantin Mikheev <mik.kon6610@gmail.com>
2026-04-02 19:15:01 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -130,6 +130,9 @@ const QuestionCard = ({
 }) => {
   const Icon = categoryIcons[question.category as keyof typeof categoryIcons] ?? StarMIcon;
 
+  if (!question.question) {
+    return null;
+  }
   return (
     <article className={appSt.card}>
       <div className={appSt.cardHead}>
